f818817e98de86610fa7ba8bc589709ece667bf5
YOUR_NAME <123628942+DeveloperGitHub8115@users.noreply.github.com>
2024-01-16 10:19:43 +0530

    first commit

---
+++ src/Component.js/About.js
@@ -0,0 +1,13 @@
+import { Alert, Container } from "react-bootstrap";
+export function About()
+{
+    return(
+        <>
+          <div>
+            <Container>
+                <Alert className="text-center">hiii</Alert>
+            </Container>
+          </div>
+        </>
+    )
+}
